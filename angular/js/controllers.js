532dc66f5e377426d78a17f6c1da2c1d29184335
Martin Lehmann <lehmar11@student.westerdals.no>
2015-02-23 12:42:04 +0100

    Show some dummy data using Angular.

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -1,1 +1,12 @@
-console.log('hi');
+angular.module('element1', [
+
+]);
+
+var app = angular.module('element1');
+
+app.controller('UsersController', function($scope) {
+    $scope.users = [
+        {username: 'Kanin'},
+        {username: 'koker'}
+    ];
+});
